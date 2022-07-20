ac3fd9f26b91b5921c7b2cf910c5016a39a2d7fa
xMaXiMuMx <samui_zaza@hotmail.com>
2022-07-20 22:38:55 +0700

    s

--- lib/screen/record.dart
+++ lib/screen/record.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:form_field_validator/form_field_validator.dart';
+import 'package:road_2_marathon/screen/display.dart';
+import 'package:road_2_marathon/screen/home.dart';
 import 'package:road_2_marathon/screen/index.dart';
 
 class Record extends StatefulWidget {
@@ -12,74 +15,84 @@ class Record extends StatefulWidget {
 }
 
 class _RecordState extends State<Record> {
-  final formKey = GlobalKey<FormState>();
+  String? selectedValue = 'Ages';
+  final auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellowAccent[100],
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Text("Your Record"),
+        title: Text("Create Your Program"),
       ),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: Form(
-          key: formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "เป้าหมายที่วางไว้",
-                  style: TextStyle(fontSize: 20),
-                ),
-                TextFormField(),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "ระยะทางที่วิ่งได้",
-                  style: TextStyle(fontSize: 20),
-                ),
-                TextFormField(),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "ระยะทางคงเหลือ",
-                  style: TextStyle(fontSize: 20),
-                ),
-                TextFormField(),
-                SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    child: Text(
-                      "บันทึกข้อมูล",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    onPressed: () {
-                      Fluttertoast.showToast(
-                          msg: "บันทึกข้อมูลเรียบร้อยแล้ว",
-                          gravity: ToastGravity.BOTTOM);
-                      Navigator.pushReplacement(context,
-                          MaterialPageRoute(builder: (context) {
-                        return Index();
-                      }));
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.black,
-                    ),
-                  ),
-                )
-              ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 50,
             ),
-          ),
+            DropdownButton<String>(
+              value: selectedValue,
+              items: ['Ages', 'Times', 'Distance']
+                  .map(
+                    (String item) => DropdownMenuItem<String>(
+                      child: Text(item),
+                      value: item,
+                    ),
+                  )
+                  .toList(),
+              onChanged: (String? value) {
+                setState(() {
+                  selectedValue = value;
+                });
+              },
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            DropdownButtonFormField<String>(
+              value: selectedValue,
+              items: ['Ages', 'Times', 'Distance']
+                  .map(
+                    (String item) => DropdownMenuItem<String>(
+                      child: Text(item),
+                      value: item,
+                    ),
+                  )
+                  .toList(),
+              onChanged: (String? value) {
+                setState(() {
+                  selectedValue = value;
+                });
+              },
+            ),
+            SizedBox(
+              height: 200,
+              if (value == 'Ages') {
+                if (value == '15-20') {
+                  return snapshot.data!.docs.map((Display))
+                }
+              }
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 50.0,
+              child: ElevatedButton.icon(
+                icon: Icon(Icons.run_circle),
+                label: Text("Simulate", style: TextStyle(fontSize: 20)),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return DisplayScreen();
+                  }));
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
